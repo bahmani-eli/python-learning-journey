16c99e500930c8dd9b91987d75e2b6da714d5ee4
bahmani-eli <bahmani.el@gmail.com>
2026-07-27 11:23:45 +0330

    add day003 practice*grad

---
+++ day003/grade.py
@@ -0,0 +1,11 @@
+score=int(input("your score:" ))
+if score>=90 and score<=100 :
+  print("Grade is A")
+elif score>=80 and score<90 :
+    print("Grade is B")
+elif score>=70 and score<80 :
+    print("Grade is C")
+elif score>=60 and score<70:
+    print("Grade is D")
+else:
+    print("Grade is F")
